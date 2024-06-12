58c1bb7d5432febb9e1fc58ce44bd09c1dcd57fe
Anthony Bui <builetuananh2708@gmail.com>
2024-06-12 16:29:04 +0700

    240612 - BLTAnh - 90DevOps D11 done

---
+++ src/content/docs/reference/golang/code/src/web/printf.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func printf() {
+	var challenge = "#90NgàyDevOps"
+	const totalDays = 90
+	var daysCompleted = 11
+
+	fmt.Printf("Chào mừng đến với %v\n", challenge)
+	fmt.Printf("Đây là hành trình %v ngày và bạn đã hoàn thành %v ngày.\n", totalDays, daysCompleted)
+	fmt.Println("Chúc bạn thượng lộ bình an!")
+}
